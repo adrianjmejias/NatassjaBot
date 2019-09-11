7053accd7e77a350604fdf336da9c54a287e5107
alosaft <alosafti@gmail.com>
2019-09-10 22:51:31 -0400

    added import for animal

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from graph import Node, Transition
 from countries import countries
+from animal import animal 
 
 act_node =  Node(
     'Estas feliz o triste?', 
     [
         Transition.simple('feliz', Node.leaf([lambda : print('feliz')])),
-        Transition.simple('triste', countries)
+        Transition.simple('triste', animal)
     ],
     [
-        lambda: print('action1'),
+        lambda: print('act_question'),
     ])
 
 
