cbcb57e1c959591d7e1aaf8c1583c6c453a809ac
adrian <adrianjmejias@gmail.com>
2019-09-13 17:10:35 -0400

    Transition.with_actions sample in animals.py

--- animal.py
+++ animal.py
@@ -2,11 +2,15 @@ from graph import Node, Transition
 from config import id_test
 
 
+node_test =  Node.leaf([lambda bot: bot.send_message(id_test, 'you moved to a test node')])
+
 animal = Node('Entonces sal a pasear. Prefieres encontrarte un…', 
     [
         Transition.simple('Perro', Node.leaf([lambda bot: bot.send_audio(id_test, open('./Audio1.mp3', 'rb'), '')])),
         Transition.simple('Gato', Node.leaf([lambda bot: bot.send_audio(id_test, open('./Audio2.mp3', 'rb'), '')])),
-        
+        Transition.with_actions('test', node_test,
+            [lambda bot: bot.send_message(id_test, 'this is shown as a Transition\'s action')]
+        )
     ],
     [
 
