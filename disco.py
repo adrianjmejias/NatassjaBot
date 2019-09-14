6e51ce532aff8b574dff5448dfcb9e02b2ffa052
alosaft <alosafti@gmail.com>
2019-09-13 20:24:30 -0400

    moved assets to assets/ folder to make everything more organized. We now build the assets path using the asset_path function

--- disco.py
+++ disco.py
@@ -1,14 +1,18 @@
 from graph import Node, Transition
 from config import id_test
+from util import asset_path
 
 
 disco = Node('Decidiste ir a la disco. ¿Qué color de sombra usarías?', 
     [
         Transition.simple('Azul', Node.leaf([lambda bot: bot.send_message(id_test, 'https://twitter.com/VitaVirginiaBot/status/1167462809430630400?s=20')])),
         Transition.simple('Rojo', Node.leaf([lambda bot: bot.send_message(id_test, 'https://www.poeticous.com/evaristo-carriego/el-clavel?locale=es')])),
-        Transition.simple('Negro', Node.leaf([lambda bot: bot.send_photo(id_test, open('./gatonegro.jpg', 'rb'), '')])),
+        Transition.simple('Negro', Node.leaf([lambda bot: bot.send_photo(id_test, open( asset_path('gatonegro.jpg'), 'rb'), '')])),
         Transition.simple('Verde', Node.leaf([lambda bot: bot.send_message(id_test, 'https://www.desmos.com/calculator/x3vloqe7lq')]))
     ],
     [
 
     ])
+
+
+
---
+++ util.py
@@ -0,0 +1,5 @@
+
+
+
+def asset_path(file_name):
+    return './assets/'+file_name
