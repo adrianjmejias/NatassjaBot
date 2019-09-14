4db91b2271b8d55f990ef963928c5583b49edf2a
alosaft <alosafti@gmail.com>
2019-09-14 00:49:02 -0400

    added signo to perrito.py flow

--- perrito.py
+++ perrito.py
@@ -1,13 +1,14 @@
 from graph import Node, Transition
 from config import id_test
+from signo import signo
 
 perrito = Node('Obviamente pasas por los perros antes de la disco, ¿qué salsa le pones?', 
     [
-        Transition.simple('De ajo', Node.leaf(
-            [lambda bot: bot.send_message(id_test, 'ajo')])
+        Transition.with_actions('De ajo', signo,
+            [lambda bot: bot.send_message(id_test, 'ajo')]
         ),
-        Transition.simple('De maiz', Node.leaf(
-            [lambda bot: bot.send_message(id_test, 'maiz')])
+        Transition.with_actions('De maiz', signo,
+            [lambda bot: bot.send_message(id_test, 'maiz')]
         ),
     ],
     [
